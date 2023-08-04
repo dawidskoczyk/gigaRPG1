cecb0c96a54760145d151689a1506b31596a7288
dawidskoczyk <dawidskoczyk@gmail.com>
2023-08-04 22:01:08 +0200

    skrzunia

---
+++ Assets/ChestOpener.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestOpener : MonoBehaviour
+{
+    public GameObject ChestClose, ChestOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChestClose.SetActive(true);
+        ChestOpen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        ChestClose.SetActive(false);
+        ChestOpen.SetActive(true);
+    }
+    void OnTriggerExit2D(Collider2D collission)
+    {
+        ChestClose.SetActive(true);
+        ChestOpen.SetActive(false);
+    }
+}
